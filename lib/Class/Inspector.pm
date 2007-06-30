634ba3721e753aec8893827161f1eb15453c3ddb
Adam Kennedy <adamk@cpan.org>
2007-06-30 01:53:31 +0000

    Removing "All rights reserved"

--- lib/Class/Inspector.pm
+++ lib/Class/Inspector.pm
@@ -618,7 +618,7 @@ L<http://ali.as/>, L<Class::Handle>
 
 =head1 COPYRIGHT
 
-Copyright (c) 2002 - 2006 Adam Kennedy. All rights reserved.
+Copyright (c) 2002 - 2006 Adam Kennedy.
 
 This program is free software; you can redistribute
 it and/or modify it under the same terms as Perl itself.
