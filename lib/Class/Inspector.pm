cade719970dc0d6ab25532024eaecfbca1c77630
Adam Kennedy <adamk@cpan.org>
2007-07-01 05:21:17 +0000

    Correcting email address

--- lib/Class/Inspector.pm
+++ lib/Class/Inspector.pm
@@ -610,7 +610,7 @@ For other issues, or commercial enhancement or support, contact the author.
 
 =head1 AUTHOR
 
-Adam Kennedy E<lt>cpan@ali.asE<gt>
+Adam Kennedy E<lt>adamk@cpan.orgE<gt>
 
 =head1 SEE ALSO
 
