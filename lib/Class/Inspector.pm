b75c0eb49f3ca10e94dca36c8c1f68978b588494
Tom Wyant <wyant@cpan.org>
2012-01-21 23:50:35 +0000

    RT #74224: loaded() noisy under Perl 5.15.7.
    The immediate problem here is that 'defined(@array)' has been deprecated
    for a while, but only with Perl 5.15.7 does Perl complain when @array is
    a package variable.
    
    The next problem is how to change the code. The standard response for
    Boolean context is just to say '@array'. This is what the patch does,
    but it was not straightforward to arrive at this. Here's the deal:
    
    The original code was 'defined @{"${name}::ISA"}. The question is
    whether this tests whether the array is occupied, or whether it tests
    whether the array has been created, independant of whether it is
    occupied.
    
    A little playing around with 'perl -E' shows that '@{"Foo::Bar"}'
    autovivifies array Bar in package Foo. That is,
      $ perl -E '@{"Foo::Bar"}; say *Foo::Bar{ARRAY}'
    prints an array reference, while
      $ perl -E 'say *Foo::Bar{ARRAY}'
    does not. So I was tempted to recast the code in terms of the glob.
    
    But
     $ perl -E 'say *Foo::ISA{ARRAY}'
    prints an array reference, so it appears the @ISA already exists, so
    that '*"${name}::ISA"{ARRAY}' would always return a true value. This
    seems to leave us with the occupancy test as the only alternative.
    
    But tricky Perl is not my strong suit, and you may well find a better
    implementation. The patch is just to try to get the ball rolling.

--- lib/Class/Inspector.pm
+++ lib/Class/Inspector.pm
@@ -49,7 +49,7 @@ use File::Spec ();
 # Globals
 use vars qw{$VERSION $RE_IDENTIFIER $RE_CLASS $UNIX};
 BEGIN {
-	$VERSION = '1.25';
+	$VERSION = '1.25_01';
 
 	# If Unicode is available, enable it so that the
 	# pattern matches below match unicode method names.
@@ -124,7 +124,7 @@ sub _loaded {
 	# Handle by far the two most common cases
 	# This is very fast and handles 99% of cases.
 	return 1 if defined ${"${name}::VERSION"};
-	return 1 if defined @{"${name}::ISA"};
+	return 1 if @{"${name}::ISA"};
 
 	# Are there any symbol table entries other than other namespaces
 	foreach ( keys %{"${name}::"} ) {
